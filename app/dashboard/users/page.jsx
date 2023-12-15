d966dee9c9ceeb4c964a8687d8934c128b0ad232
Khristian Ledezma <khristianledezma@gmail.com>
2023-12-15 15:03:06 -0400

    first commit

---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,13 @@
+import styles from "@/app/ui/dashboard/users/users.module.css"
+const Users = () =>{
+    return(
+        <>
+            <div className={styles.container}>
+                <div className={styles.top}></div>
+                <table className={styles.table}></table>
+            </div>
+        </>
+    )
+}
+
+export default Users;
---
+++ app/ui/dashboard/products/products.jsx
@@ -0,0 +1,7 @@
+import styles from "./poducts.module.css";
+
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
